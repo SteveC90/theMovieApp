77efb5f9c0b94a65a5df45c84f9233b2c2eabe52
vigonzal <vigonzal@mymail.mines.edu>
2012-09-26 14:41:48 -0600

    Added the Project to the Repo (Initial Setup nothing but hello world on it still)

---
+++ Video Collection/src/csci498P/video/collection/VideoCollection.java
@@ -0,0 +1,20 @@
+package csci498P.video.collection;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class VideoCollection extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_collection);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_video_collection, menu);
+        return true;
+    }
+}
