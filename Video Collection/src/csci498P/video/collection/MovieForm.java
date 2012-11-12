af5b69b76626355d62c32124480e66cc2af02e51
vigonzal <vigonzal@mymail.mines.edu>
2012-11-12 14:18:05 -0700

    Updated the activity_...xml and strings.xml so that they both now support the added buttons and color references. Updated some of MovieForm so that there are less commented out code blocks and instead have some meaningful code lines. Changed and added some methods to VideoCollection.java so that the buttons would be added, and each have a listener.

--- Video Collection/src/csci498P/video/collection/MovieForm.java
+++ Video Collection/src/csci498P/video/collection/MovieForm.java
@@ -1,21 +1,8 @@
 package csci498P.video.collection;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.database.Cursor;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MovieForm extends Activity {
 
@@ -49,30 +36,25 @@ public class MovieForm extends Activity {
 	@Override
 	public void onSaveInstanceState(Bundle state) {
 		super.onSaveInstanceState(state);
-
-//		state.putString("name", name.getText().toString());
-//		state.putString("address", address.getText().toString());
-//		state.putString("notes", notes.getText().toString());
-//		state.putInt("type", types.getCheckedRadioButtonId());
-//		state.putString("feed", feed.getText().toString());
+		state.putString("title", title.getText().toString());
+		state.putString("genre", genre.getText().toString());
+		state.putString("release", release.getText().toString());
 	}
 
 	@Override
 	public void onRestoreInstanceState(Bundle state) {
 		super.onRestoreInstanceState(state);
-//		name.setText(state.getString("name"));
-//		address.setText(state.getString("address"));
-//		notes.setText(state.getString("notes"));
-//		types.check(state.getInt("type"));
-//		feed.setText(state.getString("feed"));
+		title.setText(state.getString("title"));
+		genre.setText(state.getString("genre"));
+		release.setText(state.getString("release"));
 	}
 
 	private void save() {
 		//TODO
 	}
 
-	private void load() {
-		//TODO
-	}
+//	private void load() {
+//		//TODO
+//	}
 
 }
--- Video Collection/src/csci498P/video/collection/VideoCollection.java
+++ Video Collection/src/csci498P/video/collection/VideoCollection.java
@@ -2,19 +2,52 @@ package csci498P.video.collection;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class VideoCollection extends Activity {
+	
+	private View.OnClickListener onGallery = new View.OnClickListener() {
+		public void onClick(View v) {
+			//TODO
+			//startActivity(new Intent(VideoCollection.this, Gallery.class));
+		}
+	};
+	
+	private View.OnClickListener onLCScreen = new View.OnClickListener() {
+		public void onClick(View v) {
+			//TODO
+			//startActivity(new Intent(VideoCollection.this, LCScreen.class));
+		}
+	};
+	
+	private View.OnClickListener onExtra = new View.OnClickListener() {
+		public void onClick(View v) {
+			//TODO
+			//Extra might be for find movie theatre location
+			//startActivity(new Intent(VideoCollection.this, Extra.class));
+		}
+	};
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_collection);
+        
+        Button gallery = (Button)findViewById(R.id.gallery);
+        Button load_create = (Button)findViewById(R.id.load_create);
+        Button extra = (Button)findViewById(R.id.extra);
+        gallery.setOnClickListener(onGallery);
+        load_create.setOnClickListener(onLCScreen);
+        extra.setOnClickListener(onExtra);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_video_collection, menu);
-        return true;
-    }
+    //Don't know if we want a menu in the main menu screen.
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.options, menu);
+//        return true;
+//    }
 }
