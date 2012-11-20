a16b275463d0890dd8e9cc8ba20f50a92d1b5dcf
vigonzal <vigonzal@mymail.mines.edu>
2012-11-20 00:05:53 -0700

    got the archive screen to show up by editing the oncreate, started to put in the Overrides, and refactored archive to LCScreen.

--- Video Collection/src/csci498P/video/collection/Gallery.java
+++ Video Collection/src/csci498P/video/collection/Gallery.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 
 public class Gallery extends Activity{
 	
+	@Override
 	public void onCreate(Bundle savedInstanceState){
 		//TODO / to-finish really.
 		super.onCreate(savedInstanceState);
--- Video Collection/src/csci498P/video/collection/LCScreen.java
+++ Video Collection/src/csci498P/video/collection/LCScreen.java
@@ -1,12 +1,12 @@
 package csci498P.video.collection;
 
-import android.R.layout;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class LCScreen extends Activity {
 	
-	public void oncreate(Bundle savedInstanceState) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.archive);
 	}
