d2cd875cc1b662b9cf300bc0125cdf2022f1b33e
Esteban Chavez <echavez@mymail.mines.edu>
2012-11-26 14:37:46 -0700

    Added the barcode implementation. Added a barcode field to themovie form and an options menu to the form as well. The menu has scan barcode option that when selected will open the barcode app and the results are saved into the movie form

--- Video Collection/src/csci498P/video/collection/MovieForm.java
+++ Video Collection/src/csci498P/video/collection/MovieForm.java
@@ -1,7 +1,11 @@
 package csci498P.video.collection;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MovieForm extends Activity {
@@ -9,6 +13,7 @@ public class MovieForm extends Activity {
 	EditText title;
 	EditText genre;
 	EditText release;
+	EditText barcode;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,7 @@ public class MovieForm extends Activity {
 		title = (EditText)findViewById(R.id.title);
 		genre = (EditText)findViewById(R.id.genre);
 		release = (EditText)findViewById(R.id.release);
-
+		barcode = (EditText)findViewById(R.id.barcode);
 	}
 
 	@Override
@@ -32,7 +37,24 @@ public class MovieForm extends Activity {
 		super.onDestroy();
 		//helper.close();
 	}
-
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	  new MenuInflater(this).inflate(R.menu.details_option, menu);
+	  return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() == R.id.scan_barcode) {
+			//opens the barcode scanner app
+			IntentIntegrator integrator = new IntentIntegrator(this);
+			integrator.initiateScan();
+		}
+		
+		return super.onOptionsItemSelected(item);
+	}
+	
 	@Override
 	public void onSaveInstanceState(Bundle state) {
 		super.onSaveInstanceState(state);
@@ -56,5 +78,16 @@ public class MovieForm extends Activity {
 //	private void load() {
 //		//TODO
 //	}
+	
+	
+	//this piece of code handles the result of the barcode scanner
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (scanResult != null) {
+			barcode.setText(scanResult.getContents());
+		}
+		// else continue with any other code you need in the method
+		
+	}
 
 }
